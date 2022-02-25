d0dc780591263a215ff580d420ccb344a20ef2a1
Daniel Pereyra <pereyradaniell@gmail.com>
2022-02-25 10:36:53 -0300

    agregado de ventana modal

--- js/js.js
+++ js/js.js
@@ -11,3 +11,12 @@ if (snippet.length) {
     });
   });
 }
+
+
+/*modal*/
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
